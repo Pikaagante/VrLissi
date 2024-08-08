d9cc3a94374290ef1d3bafeebde0bad3551eddf1
Pikaagante <briaultmaxence@gmail.com>
2024-08-08 16:30:24 +0200

    add end for tower defense and canvas for description

--- Assets/Script/Shoot/WinCible.cs
+++ Assets/Script/Shoot/WinCible.cs
@@ -6,8 +6,26 @@ using UnityEngine.SceneManagement;
 public class WinCible : MonoBehaviour
 {
     [SerializeField] private Canvas canvasToControl;
-
     public string scenename;
+    private float timer = 120f; 
+    private bool timerStarted = false;
+
+    void Start()
+    {
+        // Start the timer on Start
+        StartCoroutine(StartTimer());
+    }
+
+    IEnumerator StartTimer()
+    {
+        timerStarted = true;
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            yield return null;
+        }
+        canvasToControl.enabled = true;
+    }
 
     public void ReturnHub()
     {
